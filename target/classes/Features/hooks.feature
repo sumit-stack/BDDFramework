358ca4ba2b34e8caedd7f4ba9d1378b4e47cc101
Sumit Sharma <sumitsharma@sumitsharmas-MacBook-Air.local>
2020-05-16 18:57:47 -0400

    Global and local hooks

---
+++ target/classes/Features/hooks.feature
@@ -0,0 +1,21 @@
+Feature: Free CRM Application
+
+@First
+Scenario: Login to CRM Application
+Given user login to CRM application
+
+@Second
+Scenario: user navigate to contacts
+Given user navigate to contacts
+
+@Third
+Scenario: user navigate to deals
+Given user navigate to deals
+
+@Fourth
+Scenario: user home page
+Given user navigate to home page
+
+@Fifth
+Scenario: user navigate to companies
+Given user navigate to companies
--- test-output/report.js
+++ test-output/report.js
@@ -1,9 +1,187 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("/Users/sumitsharma/MyStuff/BDD/FreeCRMBDDFramework/src/main/java/Features/Tagging.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("/Users/sumitsharma/MyStuff/BDD/FreeCRMBDDFramework/src/main/java/Features/hooks.feature");
 formatter.feature({
   "line": 1,
   "name": "Free CRM Application",
   "description": "",
   "id": "free-crm-application",
   "keyword": "Feature"
+});
+formatter.before({
+  "duration": 456555,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 75110,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Login to CRM Application",
+  "description": "",
+  "id": "free-crm-application;login-to-crm-application",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@First"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "user login to CRM application",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "hooksStepDefinition.user_login_to_CRM_application()"
+});
+formatter.result({
+  "duration": 131341684,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 100842,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 59163,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 138930,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 8,
+  "name": "user navigate to contacts",
+  "description": "",
+  "id": "free-crm-application;user-navigate-to-contacts",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 7,
+      "name": "@Second"
+    }
+  ]
+});
+formatter.step({
+  "line": 9,
+  "name": "user navigate to contacts",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "hooksStepDefinition.user_navigate_to_contacts()"
+});
+formatter.result({
+  "duration": 63380,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 282256,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 137855,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 12,
+  "name": "user navigate to deals",
+  "description": "",
+  "id": "free-crm-application;user-navigate-to-deals",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 11,
+      "name": "@Third"
+    }
+  ]
+});
+formatter.step({
+  "line": 13,
+  "name": "user navigate to deals",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "hooksStepDefinition.user_navigate_to_deals()"
+});
+formatter.result({
+  "duration": 81090,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 56497,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 117089,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 16,
+  "name": "user home page",
+  "description": "",
+  "id": "free-crm-application;user-home-page",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 15,
+      "name": "@Fourth"
+    }
+  ]
+});
+formatter.step({
+  "line": 17,
+  "name": "user navigate to home page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "hooksStepDefinition.user_navigate_to_home_page()"
+});
+formatter.result({
+  "duration": 66462,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 77553,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 117001,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 20,
+  "name": "user navigate to companies",
+  "description": "",
+  "id": "free-crm-application;user-navigate-to-companies",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 19,
+      "name": "@Fifth"
+    }
+  ]
+});
+formatter.step({
+  "line": 21,
+  "name": "user navigate to companies",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "hooksStepDefinition.user_navigate_to_companies()"
+});
+formatter.result({
+  "duration": 56392,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 92570,
+  "status": "passed"
 });
 });
